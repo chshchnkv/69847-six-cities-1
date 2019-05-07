fd516ec166af8afeb8ec92249f48eaed7897af9a
Stan Chashchnikov <chashchnikov@gmail.com>
2019-05-07 13:39:51 +0400

    module1-task3

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 const path = require(`path`);
 
 module.exports = {
-    entry: `./src/index.js`,
-    output: {
-        filename: `bundle.js`,
-        path: path.join(__dirname, `public`)
-    },
-    devServer: {
-        contentBase: path.join(__dirname, `public`),
-        compress: false,
-        port: 1337,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: `babel-loader`,
-                },
-            }
-        ],
-    },
-    devtool: `source-map`
+  entry: `./src/index.js`,
+  resolve: {
+    extensions: [`.js`, `.jsx`],
+    modules: [
+      `node_modules`,
+      path.resolve(`./src`)
+    ]
+  },
+  output: {
+    filename: `bundle.js`,
+    path: path.resolve(`./public`)
+  },
+  devServer: {
+    contentBase: path.resolve(`./public`),
+    compress: false,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      }
+    ],
+  },
+  devtool: `source-map`
 };
