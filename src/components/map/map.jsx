6794f1f9d847c8f7f5161a6954326c0b587c3f13
Stan Chashchnikov <chashchnikov@gmail.com>
2019-05-19 22:26:42 +0300

    module4-task1

---
+++ src/components/map/map.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import PropTypes from "prop-types";
+import * as leaflet from "leaflet";
+
+class Map extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._mapRef = React.createRef();
+
+    this._icon = leaflet.icon({
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    });
+
+  }
+
+  componentDidMount() {
+    const {
+      city,
+      zoom,
+      pins = []
+    } = this.props;
+
+    const {
+      longitude,
+      latitude
+    } = city;
+
+    this._map = leaflet.map(this._mapRef.current, {
+      center: [longitude, latitude],
+      zoom,
+      zoomControl: false,
+      marker: true
+    });
+
+    this._map.setView([longitude, latitude], zoom);
+    leaflet
+      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+      })
+      .addTo(this._map);
+
+    pins.forEach((pin) => {
+      const offerCords = [pin.longitude, pin.latitude];
+      leaflet
+        .marker(offerCords, {icon: this._icon})
+        .addTo(this._map);
+    });
+  }
+
+  componentWillUnmount() {
+    this._map = null;
+  }
+
+  render() {
+    return <div ref={this._mapRef} style={{display: `flex`, width: 100 + `%`, height: 100 + `%`}}/>;
+  }
+}
+
+Map.propTypes = {
+  city: PropTypes.shape({
+    longitude: PropTypes.number,
+    latitude: PropTypes.number
+  }).isRequired,
+  zoom: PropTypes.number.isRequired,
+  pins: PropTypes.arrayOf(PropTypes.shape({
+    longitude: PropTypes.number,
+    latitude: PropTypes.number,
+  }))
+};
+
+export default Map;
---
+++ src/components/map/map.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Map from "./map";
+
+const mock = {
+  city: {
+    longitude: 52.38333,
+    latitude: 4.9
+  },
+  zoom: 12,
+  pins: [
+    {
+      longitude: 52.3909553943508,
+      latitude: 4.85309666406198
+    },
+    {
+      longitude: 52.369553943508,
+      latitude: 4.85309666406198
+    },
+    {
+      longitude: 52.3909553943508,
+      latitude: 4.929309666406198
+    },
+    {
+      longitude: 52.3809553943508,
+      latitude: 4.939309666406198
+    },
+    {
+      longitude: 52.3809553943508,
+      latitude: 4.929309666406198
+    }
+  ]
+};
+
+it(`Map renders correctly`, () => {
+  const options = {};
+  const tree = renderer.create(<Map city={mock.city} zoom={mock.zoom} pins={mock.pins}/>, options);
+  expect(tree).toMatchSnapshot();
+});
